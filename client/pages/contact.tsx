05bec13af8e6936cf3460d4dca80d3fb5b2264b6
osawa-koki <github@osawa.dev>
2023-04-11 21:44:28 +0900

    ページネーション状況の表示。

--- client/pages/contact.tsx
+++ client/pages/contact.tsx
@@ -54,37 +54,38 @@ export default function ContactPage() {
   return (
     <Layout>
       <div id="Contact">
-
-        <Table>
-          <thead>
-            <tr>
-              <th>id</th>
-              <th>title</th>
-              <th>content</th>
-              <th>status</th>
-              <th>published_at</th>
-              <th>closed_at</th>
-            </tr>
-          </thead>
-          <tbody>
-          {contacts.contacts && Array.from({ length: 5 }).map((contact: IContact, index) => (
-            <tr key={index}>
-              {contacts.contacts[index] ? (
-                <>
-                  <td>{contacts.contacts[index].id}</td>
-                  <td>{contacts.contacts[index].title}</td>
-                  <td>{contacts.contacts[index].content}</td>
-                  <td>{contacts.contacts[index].status}</td>
-                  <td>{contacts.contacts[index].published_at.toString()}</td>
-                  <td>{contacts.contacts[index].closed_at.toString()}</td>
-                </>
-              ) : (
-                <td colSpan={6} className="invisible">Empty</td>
-              )}
-            </tr>
-          ))}
-          </tbody>
-        </Table>
+        <div className="w-100 overflow-auto">
+          <Table>
+            <thead>
+              <tr>
+                <th>id</th>
+                <th>title</th>
+                <th>content</th>
+                <th>status</th>
+                <th>published_at</th>
+                <th>closed_at</th>
+              </tr>
+            </thead>
+            <tbody>
+            {contacts.contacts && Array.from({ length: 5 }).map((contact: IContact, index) => (
+              <tr key={index}>
+                {contacts.contacts[index] ? (
+                  <>
+                    <td>{contacts.contacts[index].id}</td>
+                    <td>{contacts.contacts[index].title}</td>
+                    <td>{contacts.contacts[index].content}</td>
+                    <td>{contacts.contacts[index].status}</td>
+                    <td>{contacts.contacts[index].published_at.toString()}</td>
+                    <td>{contacts.contacts[index].closed_at.toString()}</td>
+                  </>
+                ) : (
+                  <td colSpan={6} className="invisible">Empty</td>
+                )}
+              </tr>
+            ))}
+            </tbody>
+          </Table>
+        </div>
         <div className="mt-3 d-flex justify-content-between">
           <Button variant="primary" onClick={() => {
             if (contacts.pagination.prev_page) {
@@ -98,6 +99,22 @@ export default function ContactPage() {
             }
           }} className="d-block mt-3 m-auto" size="sm" disabled={contacts.pagination.next_page === null}>次へ</Button>
         </div>
+        <Table className="border mt-3">
+          <tbody>
+            <tr>
+              <td>現在のページ</td>
+              <td>{contacts.pagination.current_page}</td>
+            </tr>
+            <tr>
+              <td>総ページ数</td>
+              <td>{contacts.pagination.total_pages}</td>
+            </tr>
+            <tr>
+              <td>総件数</td>
+              <td>{contacts.pagination.total_count}</td>
+            </tr>
+          </tbody>
+        </Table>
       </div>
     </Layout>
   );
