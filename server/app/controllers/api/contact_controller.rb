a905b6781b5ff287fcc12fb775fe491d0cf5a2af
osawa-koki <github@osawa.dev>
2023-04-11 19:54:35 +0900

    小さな修正。

--- server/app/controllers/api/contact_controller.rb
+++ server/app/controllers/api/contact_controller.rb
@@ -1,7 +1,4 @@
 class Api::ContactController < ApplicationController
-
-  limit = 30
-
   def index
     page = params[:page].presence || 1
     @contacts = Contact.order(created_at: :desc).page(page)
