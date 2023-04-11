1fbcc24da7f35d5fb041fd5941a992f510f1f8ba
osawa-koki <github@osawa.dev>
2023-04-11 19:57:42 +0900

    create

--- server/app/controllers/api/contact_controller.rb
+++ server/app/controllers/api/contact_controller.rb
@@ -1,12 +1,25 @@
 class Api::ContactController < ApplicationController
   def index
     page = params[:page].presence || 1
-    @contacts = Contact.order(created_at: :desc).page(page)
+    @contacts = Contact.order(id: :asc).page(page)
     render json: @contacts
   end
 
   def show
     @contact = Contact.find(params[:id])
     render json: @contact
+  end
+
+  def create
+    @contact = Contact.new(contact_params)
+    if @contact.save
+      render json: @contact
+    else
+      render json: @contact.errors
+    end
+  end
+
+  def contact_params
+    params.require(:contact).permit(:title, :content, :status, :published_at, :closed_at)
   end
 end
