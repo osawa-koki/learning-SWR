c043aa7c5e421756ebfe2319467e51e18d8b3305
osawa-koki <github@osawa.dev>
2023-04-11 19:54:24 +0900

    小さな修正。

--- server/app/controllers/api/contact_controller.rb
+++ server/app/controllers/api/contact_controller.rb
@@ -4,7 +4,7 @@ class Api::ContactController < ApplicationController
 
   def index
     page = params[:page].presence || 1
-    @contacts = Contact.order(created_at: :desc).page(page).per(5)
+    @contacts = Contact.order(created_at: :desc).page(page)
     render json: @contacts
   end
 end
