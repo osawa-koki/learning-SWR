db70d9a9f5a75f0f143ce28a0107d0447f6a8e92
osawa-koki <github@osawa.dev>
2023-04-11 21:58:33 +0900

    不要なメソッド消した。

--- server/app/controllers/api/contact_controller.rb
+++ server/app/controllers/api/contact_controller.rb
@@ -14,23 +14,9 @@ class Api::ContactController < ApplicationController
     }
   end
 
-  def show
-    @contact = Contact.find(params[:id])
-    render json: @contact
-  end
-
   def create
     @contact = Contact.new(contact_params)
     if @contact.save
-      render json: @contact
-    else
-      render json: @contact.errors
-    end
-  end
-
-  def update
-    @contact = Contact.find(params[:id])
-    if @contact.update(contact_params)
       render json: @contact
     else
       render json: @contact.errors
