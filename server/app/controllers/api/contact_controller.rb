ee04789c1ad94e6e62cb63bdbf3fc4754ee73707
osawa-koki <github@osawa.dev>
2023-04-11 20:12:50 +0900

    update - destroy

--- server/app/controllers/api/contact_controller.rb
+++ server/app/controllers/api/contact_controller.rb
@@ -19,6 +19,21 @@ class Api::ContactController < ApplicationController
     end
   end
 
+  def update
+    @contact = Contact.find(params[:id])
+    if @contact.update(contact_params)
+      render json: @contact
+    else
+      render json: @contact.errors
+    end
+  end
+
+  def destroy
+    @contact = Contact.find(params[:id])
+    @contact.destroy
+    render json: @contact
+  end
+
   def contact_params
     params.require(:contact).permit(:title, :content, :status, :published_at, :closed_at)
   end
