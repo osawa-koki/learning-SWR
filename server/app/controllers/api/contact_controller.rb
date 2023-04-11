97f92bafb68de3888935b85f13ef58999f147ea6
osawa-koki <github@osawa.dev>
2023-04-11 19:48:59 +0900

    kaminari - pagenation

--- server/app/controllers/api/contact_controller.rb
+++ server/app/controllers/api/contact_controller.rb
@@ -1,6 +1,10 @@
 class Api::ContactController < ApplicationController
+
+  limit = 30
+
   def index
-    @contacts = Contact.all
+    page = params[:page].presence || 1
+    @contacts = Contact.order(created_at: :desc).page(page).per(5)
     render json: @contacts
   end
 end
